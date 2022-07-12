99183ab3a1f55f0082c6dc9393f406613fd7c77b
Svskiv21 <natalia.krukar@gmail.com>
2022-07-12 22:06:18 +0200

    Initial commit

---
+++ src/main/java/com/example/planting_support/PlantingSupportApplication.java
@@ -0,0 +1,13 @@
+package com.example.planting_support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantingSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantingSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/planting_support/PlantingSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planting_support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantingSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
